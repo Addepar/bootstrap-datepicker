e1f2670544afd1f0440a4972d2e70d1e6feca9ad
Joseph Chu <joseph.chu@addepar.com>
2014-09-10 00:31:54 -0700

    Revert "Stop the propagation of the click event to prevent the parent handlers"
    This reverts commit fd05cf569efe61757f327bf3f6743c6330fb1802.

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -896,7 +896,6 @@
 
 		click: function(e){
 			e.preventDefault();
-			e.stopPropagation();
 			var target = $(e.target).closest('span, td, th'),
 				year, month, day;
 			if (target.length === 1){
